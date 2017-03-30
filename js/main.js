acce1326d4c12466372dae62b8b413ecaef0a69b
ty <iamtylerwarren@gmail.com>
2017-03-30 01:38:12 -0700

    completed all 3 widgets working as intended

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 (function() {
+
+// Cost widget code   
     function widgetCost(numberOfWidgets){
         var calculatedTotal = numberOfWidgets * 1.50
         return calculatedTotal
@@ -10,4 +12,54 @@
     
     });
 
+// Modal Section code
+
+let modal = document.querySelector(".modal");
+let modalBtn = document.querySelector(".displayModalBtn");
+let closeBtn = document.querySelector(".close");
+
+
+modalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Counter widget code
+
+
+
+
+
+
 })();
+
+let counter = document.querySelector(".counter"),
+    addButton = document.querySelector(".addCount"),
+    minusButton = document.querySelector(".minusCount");
+    counter.value = 0;
+
+
+addButton.addEventListener('click',add);
+minusButton.addEventListener('click', minus)
+
+function add(){
+    console.log()
+    if(!(parseInt(counter.value) === 99)){
+        counter.value = parseInt(counter.value) + 1;
+    }
+}
+
+function minus(){
+    if(!(parseInt(counter.value) === 0)){
+        counter.value = parseInt(counter.value) - 1;
+    }
+}
